fc43ab8744bc4cefdb8a9df1057b93f2de463935
mrmensah <tbmensah98@gmail.com>
2022-05-12 21:54:50 +0000

    Arduino code dir

---
+++ smart_socket/smart_socket.ino
@@ -0,0 +1,21 @@
+#define CURRENT A0  // Analog pin for nodeMCU
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(CURRENT, INPUT);   // setting A0 as read
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+
+// Measuring Current
+float current() {
+  float resolution = 3.3 / 1024; // Input Voltage Range is 1V to 3.3V
+                                 // ESP8266 ADC resolution is 10-bit. 2^10 = 1024
+  float rawCurrent = analogRead(CURRENT);
+  float actCurrent = rawCurrent * resolution;
+  Serial.print(actCurrent, 3);
+}
